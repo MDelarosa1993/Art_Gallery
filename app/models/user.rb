4f6b743f5d2cc194776e9e2ef80812b35be603b3
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-01-06 20:28:53 -0500

    Added the should matchers gem to test validations for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 
   enum role: { artist: 0, buyer: 1, admin: 2 }
 
-  
+  validates :name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true
 end
