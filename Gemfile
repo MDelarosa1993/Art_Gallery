ac7d02a7cd4c70ded1a376b0a00f2284049cdfcf
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-01-08 16:13:46 -0500

    set up models and associations and tests as well

--- Gemfile
+++ Gemfile
@@ -45,7 +45,6 @@ group :development, :test do
   gem "brakeman", require: false
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
-  gem "rubocop-rails-omakase", require: false
 end
 
 
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do 
+    it { should belong_to(:order) }
+    it { should belong_to(:artwork) }
+  end
 end
