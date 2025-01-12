4b45888e23b9a5c6ea47b2df1202909868e6cfc5
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-01-12 13:19:52 -0500

    Seeded some user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,5 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+User.create!(name: "Alonnah", email: "alonnah@example.com", password: "alonnah123", role: :artist)
