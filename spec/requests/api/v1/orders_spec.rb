ac08ece175fdcb11f14faf2a8c10f1f34ff1aba8
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-01-15 16:53:39 -0500

    added an index action for orders and upated the artworks index action

--- spec/requests/api/v1/orders_spec.rb
+++ spec/requests/api/v1/orders_spec.rb
@@ -44,4 +44,42 @@ RSpec.describe "Api::V1::Orders", type: :request do
       expect(order[:order_items][0][:updated_at]).to be_a(String)
     end
   end
+
+  describe "GET/index" do
+    it "should retreive a list of all the orders of a buyer" do 
+      user = User.create!(name: "Mel", email: "mel@example.com", password: "melchor08", role: :buyer)
+
+      post "/api/v1/auth", params: { email: user.email, password: user.password }
+      
+      expect(response).to be_successful
+
+      token = JSON.parse(response.body)["token"]
+      
+      expect(token).to_not be_nil
+      expect(token).to be_a(String)
+      expect(token.split('.').length).to eq(3)
+
+      artwork = Artwork.create!(title: "Painting", description: "Beautiful artwork", price: 600000, user_id: user.id)
+      Order.create!(user_id: user.id, total_price: 3000000, order_items_attributes: [{ artwork_id: artwork.id, quantity: 5, price: 600000 }])
+
+      get "/api/v1/users/#{user.id}/orders", headers: { "Authorization" => "Bearer #{token}" }, as: :json
+      
+      expect(response).to be_successful
+
+      orders = JSON.parse(response.body, symbolize_names: true)
+      
+
+      expect(orders[0][:id]).to be_an(Integer) 
+      expect(orders[0][:user_id]).to be_an(Integer)
+      expect(orders[0][:total_price]).to be_a(String)
+      expect(orders[0][:order_items]).to be_an(Array)
+      expect(orders[0][:order_items].first).to include(
+        id: be_an(Integer),
+        order_id: be_an(Integer),
+        artwork_id: be_an(Integer),
+        quantity: be_an(Integer),
+        price: be_a(String)
+      )
+    end
+  end
 end
